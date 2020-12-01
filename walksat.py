d318b72d123d169c9a4d8c06aa87caeb00eb229e
thtran97 <kuro@Kuro-PC.localdomain>
2020-12-01 16:59:54 +0100

    Add WalkSAT solver

--- walksat.py
+++ walksat.py
@@ -1,111 +1,29 @@
-from dimacs_parser import parse
+''''''
+
+# References
+# [1] B. Selman, H. Levesque, and D. Mitchell, “A New Method for Solving Hard Satisfiability Problems,” Proc. Tenth Natl. Conf. Artif. Intell., no. July, pp. 440–446, 1992.
+
+''''''
+
+from base_solver import Base_Solver
 import numpy as np
 import random
 import time
+from itertools import chain
 
-class WalkSAT_Solver:
-
-    def __init__(self, input_cnf_file, verbose):
-        self.list_clauses, self.nvars = parse(input_cnf_file, verbose)
-        self.verbose = verbose 
-        self.assignment = []
-        self.pool = dict() #key: literal -> element: index of clause which contains literal 
-        self.unsat_clauses = [] # save id of unsat clause
-        self.costs = np.zeros(len(self.list_clauses)) #compute nb of literals make clause true (i.e. for clause Ci, if fi>0 => T, fi==0 => F)
-        self.MAX_TRIES = 100
-        self.MAX_FLIPS = 500
-        self.nb_tries = 0
-        self.nb_flips = 0 
-        self.is_sat = False
-        self.noise_parameter = 0.2
-
-        for clause in self.list_clauses:
-            assert len(clause) > 0
-
-    def generate(self):
-        self.assignment = []
-        self.nb_tries += 1
-        self.nb_flips = 0
-        for x in range(1, self.nvars+1):
-            choice = [-1,1]
-            self.assignment.append(x * random.choice(choice))
-
-    def initialize_pool(self): 
-        for i, clause in enumerate(self.list_clauses):
-            for literal in clause:
-                if literal in self.pool.keys():
-                    self.pool[literal].append(i)
-                else:
-                    self.pool[literal] = [i]
-
-    def initialize_cost(self):
-        # Compute nb of literals make clause true (i.e. for clause Ci, if fi>0 => T, fi==0 => F)
-        # Let's call it cost ! 
-        assert len(self.assignment) > 0
-        self.unsat_clauses = []
-        for i, clause in enumerate(self.list_clauses):
-            self.costs[i] = 0
-            for literal in clause:
-                if literal in self.assignment:
-                    self.costs[i] += 1
-            if self.costs[i] == 0: #Clause[i] is currently UNSAT
-                self.unsat_clauses.append(i)
+class WalkSAT(Base_Solver):
     
-    def check(self):
-        # check if all is SAT 
-        return len(self.unsat_clauses) == 0
+    def __init__(self, input_cnf_file, verbose, SKC = True, random_walk = False, noise_parameter = 0.2):
+        super(WalkSAT, self).__init__(input_cnf_file, verbose)
+        self.SKC = SKC
+        self.random_walk = random_walk
+        self.noise_parameter = noise_parameter
 
     def pick_unsat_clause(self):
         assert len(self.unsat_clauses) > 0
         random_index = random.choice(self.unsat_clauses)
         return self.list_clauses[random_index]
 
-    def evaluate_breakcount(self, literal, bs=1, ms=1):
-        # Compute the breakcount score: #clause which turn SAT -> UNSAT
-        if literal in self.assignment:
-            original_literal = literal
-        elif -literal in self.assignment:
-            original_literal = -literal
-        # when flipping literal -> -literal
-        # For every clause which contains literal => cost--
-        breakcount = 0
-        if original_literal in self.pool.keys():
-            for i in self.pool[original_literal]:
-                if self.costs[i] == 1:
-                    breakcount += 1
-        # For every clause which contains -literal => cost ++
-        makecount = 0 
-        if -original_literal in self.pool.keys():
-            for j in self.pool[-original_literal]:
-                if self.costs[j] == 0:
-                    makecount += 1
-        # Score = break - make
-        score = bs*breakcount - ms*makecount
-        return score
-
-    def flip(self, literal):
-        self.nb_flips += 1
-        # Flip variable in assignment
-        if literal in self.assignment:
-            ind = self.assignment.index(literal)
-        elif -literal in self.assignment:
-            ind = self.assignment.index(-literal) 
-        old_literal = self.assignment[ind]
-        self.assignment[ind] *= -1
-        # Update cost
-        # Clause contains literal => cost --
-        if old_literal in self.pool.keys():
-            for i in self.pool[old_literal]:
-                self.costs[i] -= 1
-                if self.costs[i] == 0: # if SAT -> UNSAT: add to list of  unsat clauses
-                    self.unsat_clauses.append(i)
-        # Clause contains -literal => cost ++
-        if -old_literal in self.pool.keys():
-            for j in self.pool[-old_literal]:
-                if self.costs[j] == 0: # if UNSAT -> SAT: remove from list of unsat clauses
-                    self.unsat_clauses.remove(j)
-                self.costs[j] += 1
-            
     def solve(self):
         initial =  time.time()
         self.initialize_pool()
@@ -117,19 +35,28 @@ class WalkSAT_Solver:
                     self.is_sat = True
                 else:
                     assert len(self.unsat_clauses) > 0 
-                    # Choose a variable x to flip
+                    # WalkSAT idea 
+                    # Among all variables that occur in unsat clauses, pick one randomly ! (=> Diverisification)
+                    # Choose a variable x which minimizes break count in this unsat clause to flip
                     unsat_clause = self.pick_unsat_clause()
                     break_count = []
                     for literal in unsat_clause:
-                        break_count.append(self.evaluate_breakcount(literal))
-                    # if 0 in break_count: # that's an excellent x 
-                    #     x = unsat_clause[break_count.index(0)]
-                    # else:
-                    p = random.random()
-                    if p < self.noise_parameter: # pick x randomly from unsat clause
-                        x = random.choice(unsat_clause)
-                    else: 
-                        x = unsat_clause[np.argmin(break_count)]
+                        break_count.append(self.evaluate_breakcount(literal, bs=1, ms=0))
+                    
+                    # Original WalkSAT proposed by Selman, Kautz, and Cohen (1994).
+                    if self.SKC and (0 in break_count):
+                        x = unsat_clause[break_count.index(0)]
+                    else:
+                        # Random walk
+                        if self.random_walk:
+                            p = random.random()
+                            if p < self.noise_parameter: # pick x randomly from literals in all unsat clause
+                                x = random.choice(unsat_clause)
+                            else: 
+                                x = unsat_clause[np.argmin(break_count)]
+                        else:
+                            x = unsat_clause[np.argmin(break_count)]
+                    
                     self.flip(x) 
 
         end = time.time()
@@ -142,3 +69,5 @@ class WalkSAT_Solver:
         else:
             print('UNKNOWN')
             return None
+
+    
